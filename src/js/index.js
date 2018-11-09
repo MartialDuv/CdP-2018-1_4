2210b83a48b1a90ca04ac1eca1d54463e5577cfa
Martial Duverneix <mduverneix@emi.u-bordeaux1.fr>
2018-11-09 14:29:38 +0100

    ajout des fichiers .js et formulaire plus jolie

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+document.getElementById("button-redirect").addEventListener('click', redirect_creaprojet);
+
+function redirect_creaprojet() {
+    window.location.href = "../html/creaproject.html";
+}
