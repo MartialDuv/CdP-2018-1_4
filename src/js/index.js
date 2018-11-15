fe4c09bfda78149da10cbd4fc07652794c7542b5
MartialDuv <33746332+MartialDuv@users.noreply.github.com>
2018-11-15 17:15:46 +0100

    Retrait dossier inutile + ajout edit_us.ejs + modification mineures

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("button-redirect").addEventListener('click', redirect_creaprojet);
-
-function redirect_creaprojet() {
-    window.location.href = "../html/creaproject.html";
-}
