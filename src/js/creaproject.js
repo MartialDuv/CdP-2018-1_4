c87cc3eb10f6c30ecdfc4c3a1504a26fbded587f
Mary PASCAL <mary.emilie@outlook.fr>
2018-11-22 15:18:05 +0100

    modif pour ajout projet dans bdd mais ne fonctionne pas

---
+++ src/js/creaproject.js
@@ -0,0 +1,29 @@
+ const checkCreaproj = document.getElementById("btncreaproj")
+
+function checkAndAdd() {
+  // let name_proj = document.querySelector("#InputProjectName");
+  // let theme_proj = document.querySelector("#InputTheme");
+  let name = document.getElementById("InputProjectName");
+  let thematic = document.getElementById("InputTheme");
+  let owner = document.getElementById("InputOwner");
+  let client = document.getElementById("InputClient");
+  let nb_dev = document.getElementById("InputNbDeveloper");
+  let estimate_time = document.getElementById("InputDuration");
+
+  if(name.value === "" || thematic.value === ""){
+      //$('#exampleModal').modal('show');
+      alert("Vous n'avez pas rempli tous les champs !");
+  }
+  else {
+    console.log("ok !");
+    // // document.getElementById("button-create").addEventListener("click", function(){
+    //
+    document.location.href="/project";
+ }
+
+}
+
+// checkCreaproj.addEventListener('click',check);
+checkCreaproj.addEventListener('click',function() {
+  checkAndAdd();
+});
--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
+const bodyParser = require('body-parser')
 const express = require('express');
-
 const app = express();
 
+let path = require("path");
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname, '/js')))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const pg = require('pg');
+const connectionString = 'postgres://localhost:5432/docker';
 
 app.get('/', function(req, res) {
 res.render('index');
@@ -13,6 +21,26 @@ console.log("running");
 app.get('/creaproject', function(req, res) {
   res.render('creaproject');
 });
+
+app.post('/creaproject/project', function(req, res) {
+//  res.render('project');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const data = {text: req.body.text, complete: false};
+  console.log(data);
+  console.log(req.body.text)
+  res.end('Salut tout le monde !');
+
+  // const { name, amount } = req.body || {};
+  // const client = new pg.Client(connectionString);
+  // client.connect();
+  // const query = client.query(
+  //   'INSERT INTO project (name, thematic, owner, client, nb_dev, estimate_time) VALUES ($1, $2, $3, $4, $5, $6)',
+  //   [name, thematic, owner, client, nb_dev, estimate_time]);
+  // query.on('end', () => { client.end(); });
+
+});
+
 
 app.get('/project', function(req, res) {
   res.render('project');
