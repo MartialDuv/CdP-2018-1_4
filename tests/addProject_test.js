22387e983d754f3833764d82c7b24e31ac9003c5
Mary PASCAL <mary.emilie@outlook.fr>
2018-11-23 15:04:59 +0100

    debug tests 3528

--- tests/addProject_test.js
+++ tests/addProject_test.js
@@ -16,45 +16,46 @@ const args = [
     "--no-sandbox",
 ];
 
-// beforeEach(async () => {
-//   browser = await puppeteer.launch(args);
-//   const url = 'http://localhost:3000/creaproject';
-//   page = await browser.newPage();
-//   await page.goto(url);
-// });
-//
-// it("Test add project", async () => {
-//   await page.waitForSelector('.creaproject-form');
-//   await page.click("input[type=projectName]");
-//   await page.type("input[type=projectName]", project.name);
-//   await page.click("input[type=theme]");
-//   await page.type("input[type=theme]", project.theme);
-//   await page.click("button[type=button]");
-//   // Wait if we get redirected to good page
-//   await page.waitForNavigation();
-// });
+beforeEach(async () => {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+  browser = await puppeteer.launch(args);
+  const url = 'http://localhost:3000/creaproject';
+  page = await browser.newPage();
+  await page.goto(url);
+});
+
+it("Test add project", async () => {
+  await page.waitForSelector('.creaproject-form');
+  await page.click("input[type=projectName]");
+  await page.type("input[type=projectName]", project.name);
+  await page.click("input[type=theme]");
+  await page.type("input[type=theme]", project.theme);
+  await page.click("button[type=button]");
+  // Wait if we get redirected to good page
+  await page.waitForNavigation();
+});
 //
 // afterAll(() => {
 // })
 // });
 
-beforeEach(function(done) {
-  browser = puppeteer.launch(args);
-  const url = 'http://localhost:3000/creaproject';
-  page = browser.newPage();
-  page.goto(url);
-  done();
-});
-
-it("Test add us", function(done) {
-  page.waitForSelector('.creaproject-form');
-  page.click("input[type=projectName]");
-  page.type("input[type=projectName]", project.name);
-  page.click("input[type=theme]");
-  page.type("input[type=theme]", project.theme);
-  page.click("button[type=button]");
-
-  // Wait if we get redirected to good page
-  page.waitForNavigation();
-  done();
-});
+// beforeEach(function(done) {
+//   browser = puppeteer.launch(args);
+//   const url = 'http://localhost:3000/creaproject';
+//   page = browser.newPage();
+//   page.goto(url);
+//   done();
+// });
+//
+// it("Test add us", function(done) {
+//   page.waitForSelector('.creaproject-form');
+//   page.click("input[type=projectName]");
+//   page.type("input[type=projectName]", project.name);
+//   page.click("input[type=theme]");
+//   page.type("input[type=theme]", project.theme);
+//   page.click("button[type=button]");
+//
+//   // Wait if we get redirected to good page
+//   page.waitForNavigation();
+//   done();
+// });
